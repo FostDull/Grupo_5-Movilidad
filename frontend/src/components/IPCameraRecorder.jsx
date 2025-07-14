d386e2487bbcc5c7b55899ef134218bf265162d3
FostDull <jhon_villac@hotmail.com>
2025-07-14 14:45:10 -0500

    nuevos cambios

---
+++ frontend/src/components/IPCameraRecorder.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const IPCameraRecorder = () => {
+  return (
+    <div className="text-gray-800">
+      <h1 className="text-2xl font-bold">Componente de Grabación de Cámara IP</h1>
+      <p>Este componente pronto tendrá funcionalidad para grabar.</p>
+    </div>
+  );
+};
+
+export default IPCameraRecorder;
